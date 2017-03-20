0a4015bc15efab00216a09fc27f6f36c86a0fa74
philippzalewski <kontakt@centrum-ngo.pl>
2017-03-20 15:47:09 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 // plik scripts.js
 
 var buttonElem = document.getElementById('addElem'),
-    ulElem = document.getElementById('list');
+    ulElem = document.getElementById('list'),
+	q = document.getElementsByTagName('li').length;
 
 buttonElem.addEventListener('click', function(e) {
-	ulElem.innerHTML += "<li>item</li>"
-	
-	var q = ulElem.length;
-	console.log(q);
-	
-	var itemsByTagName = document.getElementsByTagName('li');
-	itemsByTagName.innerText += [q];
 
+	ulElem.innerHTML += '<li>item '+q+'</li>'
+	q++
+	
 });
